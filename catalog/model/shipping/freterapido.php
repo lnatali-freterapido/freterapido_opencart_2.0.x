6b4357c9dd58a2ba59e5e41065eda1397789971c
Andersom <dev.andersom@aticonsultoria.com>
2017-03-03 10:50:29 -0300

    Alterado o código da plataforma

--- catalog/model/shipping/freterapido.php
+++ catalog/model/shipping/freterapido.php
@@ -40,7 +40,7 @@ class ModelShippingFreteRapido extends Model
         try {
             $shipping = new FreterapidoShipping([
                 'token' => $this->config->get('freterapido_token'),
-                'codigo_plataforma' => 'opencart2',
+                'codigo_plataforma' => '58b9732e2',
                 'custo_adicional' => $this->config->get('freterapido_post_cost') ?: 0,
                 'prazo_adicional' => $this->config->get('freterapido_post_deadline') ?: 0,
                 'percentual_adicional' => $this->config->get('freterapido_additional_percentage') / 100,
