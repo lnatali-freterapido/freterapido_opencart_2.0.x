94c36fcb3b103f2f5f2fcac8b9d348827e456dc5
Andersom <andersom.rfd@hotmail.com>
2017-05-23 09:13:05 -0300

    Envia o número do pedido para API na contratação do frete

--- catalog/controller/shipping/freterapido.php
+++ catalog/controller/shipping/freterapido.php
@@ -60,6 +60,7 @@ class ControllerShippingFreterapido extends Controller {
         try {
             $hire_shipping = new FreterapidoHireShipping($token);
             $response = $hire_shipping
+                ->add_order($order_id)
                 ->add_sender(array('cnpj' => $cnpj))
                 ->add_receiver(array(
                     'cnpj_cpf' => $cpf,
