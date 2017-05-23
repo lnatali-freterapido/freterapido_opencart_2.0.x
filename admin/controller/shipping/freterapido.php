65c56a49306bfe6e4cc72e1409cbc40eec8b0a78
Andersom <andersom.rfd@hotmail.com>
2017-05-23 10:07:48 -0300

    Valor mínimo para exibir frete grátis para o frete mais barato

--- admin/controller/shipping/freterapido.php
+++ admin/controller/shipping/freterapido.php
@@ -198,6 +198,7 @@ class ControllerShippingFreteRapido extends Controller {
         $data['entry_results']= $this->language->get('entry_results');
         $data['entry_limit']= $this->language->get('entry_limit');
         $data['entry_free_shipping']= $this->language->get('entry_free_shipping');
+        $data['entry_min_value_free_shipping']= $this->language->get('entry_min_value_free_shipping');
         $data['entry_dimension']= $this->language->get('entry_dimension');
         $data['entry_length']= $this->language->get('entry_length');
         $data['entry_width']= $this->language->get('entry_width');
@@ -280,6 +281,12 @@ class ControllerShippingFreteRapido extends Controller {
             $data['freterapido_free_shipping'] = $this->request->post['freterapido_free_shipping'];
         } else {
             $data['freterapido_free_shipping'] = $this->config->get('freterapido_free_shipping');
+        }
+
+        if (isset($this->request->post['freterapido_min_value_free_shipping'])) {
+            $data['freterapido_min_value_free_shipping'] = $this->request->post['freterapido_min_value_free_shipping'];
+        } else {
+            $data['freterapido_min_value_free_shipping'] = $this->config->get('freterapido_min_value_free_shipping');
         }
 
         if (isset($this->request->post['freterapido_msg_prazo'])) {
